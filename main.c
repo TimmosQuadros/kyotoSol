2465818000cc2dc1512dda238a354890d85a0ec2
Timm Rasmussen <timm.rasmussen@gmail.com>
2022-04-28 21:17:30 +0200

    Implementerede ld funktionen så man kan loade et spil kort fra en fil :-)...

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define MAXCHAR 1000;
+
 //Structure definition
 typedef struct card Card;
 struct card{
@@ -29,6 +31,8 @@ Card *CreateCard(char val, char sui, char * col, char * fie, bool vis){
     newCard->visible = vis;
     return newCard;
 }
+
+
 
 typedef struct deck Deck;
 struct deck{
@@ -101,25 +105,182 @@ const char standardDeck[52][2] =
                 "AS"
         };
 
+void shuffle(char arr[52][2], int x_size, int y_size){
+
+    srand(10);
+    int upper = x_size-1;
+    int lower = 0;
+    for (int i = 0; i < x_size; i++) {
+        int i1 = (rand() % (upper - lower + 1)) + lower;
+        char t1 = arr[i][0];
+        char t2 = arr[i][1];
+        arr[i][0] = arr[i1][0];
+        arr[i][1] = arr[i1][1];
+        arr[i1][0] = t1;
+        arr[i1][1] = t2;
+    }
+}
+
+Card * ld(char filename[]){
+    FILE *fp;
+    char row[1000];
+    char *token;
+
+    fp = fopen(filename,"r");
+
+    Card *newCard = NULL;
+    Card *previous_card = NULL;
+    Card *linked_list = NULL;
+    while (feof(fp) != true)
+    {
+        fgets(row, 1000, fp);
+        //printf("Row: %s", row);
+
+        token = strtok(row, "\n"); //Separator
+
+        while(token != NULL)
+        {
+            if(newCard!=NULL)
+                previous_card = newCard;
+            char value = *token;
+            token++;
+            char suit = *token;
+            token = NULL;
+            newCard = CreateCard(value,suit,NULL,NULL,NULL);
+            if(previous_card!=NULL){
+                previous_card->next=newCard;
+            }else{
+                linked_list = newCard;
+            }
+        }
+
+    }
+    return linked_list;
+}
 
 
 int main() {
     char stdDeck[52][2];
     strncpy(stdDeck,standardDeck,104);
+    //shuffle(stdDeck,52,2);
+    Card *linked_list = NULL;
+    linked_list = ld("/Users/timmrasmussen/Desktop/cardDeck");
 
-    Card *newCard = NULL;
+    Card *card = NULL;
+    card = linked_list;
+    while(card!=NULL){
+        printf("%c%c\n",card->value,card->suit);
+        card = card->next;
+    }
+    //Card *newCard = NULL;
 
-    newCard = CreateCard('A','S',"C1",NULL,true);
+    //newCard = CreateCard('A','S',"C1",NULL,true);
 
-    printf("Successful add of flight %c",newCard->value);
-
-    /*for(int i = 0; i<52; i++){
+    //printf("Successful add of flight %c",newCard->value);
+    /*int count=0;
+    for(int i = 0; i<52; i++){
         for(int j = 0; j<2; j++){
-            printf("%c ",stdDeck[i][j]);
+            printf("%c",stdDeck[i][j]);
         }
+        //printf(" %d", count);
+        count++;
         printf("\n");
     }*/
+
+    /*printf("%s\t%s\t%s\t%s\n","value","Column","Field","Visible");
+
+
+    for(int i = 0; i<52; i++){
+        if(i==0){
+            printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+            printf("\t%s","C1");
+            printf("\t%s","NULL");
+            printf("\t%s\n","true");
+        }
+        if(i>0 && i<7){
+            if(i==1){
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C2");
+                printf("\t%s","NULL");
+                printf("\t%s\n","false");
+            }else{
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C2");
+                printf("\t%s","NULL");
+                printf("\t%s\n","true");
+            }
+        }
+        if(i>6 && i<14){
+            if(i<9){
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C3");
+                printf("\t%s","NULL");
+                printf("\t%s\n","false");
+            }else{
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C3");
+                printf("\t%s","NULL");
+                printf("\t%s\n","true");
+            }
+        }
+        if(i>13 && i<22){
+            if(i<17){
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C4");
+                printf("\t%s","NULL");
+                printf("\t%s\n","false");
+            }else{
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C4");
+                printf("\t%s","NULL");
+                printf("\t%s\n","true");
+            }
+        }
+        if(i>21 && i<31){
+            if(i<26){
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C5");
+                printf("\t%s","NULL");
+                printf("\t%s\n","false");
+            }else{
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C5");
+                printf("\t%s","NULL");
+                printf("\t%s\n","true");
+            }
+        }
+        if(i>30 && i<41){
+            if(i<36){
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C6");
+                printf("\t%s","NULL");
+                printf("\t%s\n","false");
+            }else{
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C6");
+                printf("\t%s","NULL");
+                printf("\t%s\n","true");
+            }
+        }
+        if(i>40 && i<52){
+            if(i<47){
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C7");
+                printf("\t%s","NULL");
+                printf("\t%s\n","false");
+            }else{
+                printf("%c%c",stdDeck[i][0],stdDeck[i][1]);
+                printf("\t%s","C7");
+                printf("\t%s","NULL");
+                printf("\t%s\n","true");
+            }
+        }
+    }*/
+
+
 
     //printf("Hello, World!\n");
     return 0;
 }
+
+
